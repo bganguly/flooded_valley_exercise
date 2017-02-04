611fb8e1d8f9ad530117d82110ccd35e0878ef70
bganguly <gangulybikramjit@hotmail.com>
2017-02-04 10:54:26 -0800

    add coderbyte problems - first pass

--- coderbyte_problems.js
+++ coderbyte_problems.js
@@ -14,9 +14,9 @@ let fizzbuzz = (n) => {
     }
     return result;
 };
-console.log(fizzbuzz(3)[2] === 'Fizz');
-console.log(fizzbuzz(5)[4] === 'Buzz');
-console.log(fizzbuzz(15)[14] === 'FizzBuzz');
+console.log('fizzbuzz',fizzbuzz(3)[2] === 'Fizz');
+console.log('fizzbuzz',fizzbuzz(5)[4] === 'Buzz');
+console.log('fizzbuzz',fizzbuzz(15)[14] === 'FizzBuzz');
 
 let twoSum = (arr, S) => {
     let hashTable = {};
@@ -29,10 +29,12 @@ let twoSum = (arr, S) => {
     }
     return false;
 };
-console.log(twoSum([1,2,3],5) === true);
-console.log(twoSum([1,2,3],6) === false);
+console.log('twoSum',twoSum([1,2,3],5) === true);
+console.log('twoSum',twoSum([1,2,3],6) === false);
 
-let sumOfNestedArray = (arr) => {
+// let does not work for recursion calls, using block scope
+// let sumOfNestedArray = (arr) => {
+var sumOfNestedArray = function (arr) {
     let result = 0;
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] !== 'number') {
@@ -43,10 +45,13 @@ let sumOfNestedArray = (arr) => {
     }
     return result;
 };
+console.log('sumOfNestedArray',sumOfNestedArray([1,2,[1,2,3]]) === 9);
+console.log('sumOfNestedArray',sumOfNestedArray([1,2,3]) === 6);
 
-function clockAngle(hour, min) {
+let clockAngle = (hour, min) => {
     var h = 0.5 * (60 * hour + min);
     var m = 6 * min;
     var angle = Math.abs(h - m);
     return (angle > 180) ? 360 - angle : angle;
-}
+};
+console.log('clockAngle',clockAngle(3,0) === 90);
